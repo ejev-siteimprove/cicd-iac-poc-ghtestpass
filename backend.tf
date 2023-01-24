efdfded1b86fa461fe051428581284d706d4a638
ejev-siteimprove <116016175+ejev-siteimprove@users.noreply.github.com>
2023-01-24 12:22:59 -0600

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,13 +1,9 @@
 terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
+  cloud {
+    organization = "FusionIT"
+
+    workspaces {
+      name = "cicd-iac-poc-ghtestpass"
     }
   }
-}
-
-# Configure the AWS Provider
-provider "aws" {
-  region = "ca-central-1"
 }
